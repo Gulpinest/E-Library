43c87d68e2e307c8c1cb761d13b71f7f153b10dc
Irfan Setiawan <almyratrisha18@gmail.com>
2022-06-28 16:48:29 +0700

    View Login dan Register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,91 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    body{
+        background-color: #f3f5f7;
+    }
+
+    .lBox{
+        min-width: 0;
+        background-color: white;
+        box-shadow: #c7c7c7 0px 0px 30px;
+        padding: 30px;
+        border-radius: 15px;
+    }
+
+    .btnSubmit {
+        border-radius: 15px; 
+        padding: 5px 20px;
+        border: none;
+        color: white;
+        background-image: linear-gradient(273deg, #4eb3ff, #0074ff);
+        box-shadow: #c7c7c7 5px 0px 20px;
+        width: 100%;
+    }
+
+    input{
+        border-radius: 15px !important;
+        margin-bottom: 10px;    
+    }
+
+    .fPass{
+        float: right;
+        font-size: 12px;
+        color: rgb(109, 109, 109);
+        margin-top: 10px;
+        text-decoration: none;
+    }
+    
+</style>
+
 <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-xl-4 col-lg-6">
+            <div class="lBox">
+                <h1 class="text-center">Welcome Back</h1><br>
+
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <label for="" class="text-bold"><strong>Email</strong></label>
+                    <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus name="email" id="">
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+
+                    <label for="" class=""><strong>Password</strong></label>
+                    <input type="password" name="password" id="" class="form-control @error('password') is-invalid @enderror" required autocomplete="current-password">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+
+                    <div class="form-check" style="float: left; margin-top: 10px;">
+                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                        <label class="form-check-label" for="remember">
+                            {{ __('Remember Me') }}
+                        </label>
+                    </div>
+                    
+                    @if (Route::has('password.request'))
+                        <a class="fPass" href="{{ route('password.request') }}">
+                            {{ __('Forgot Your Password?') }}
+                        </a><br><br><br>   
+                    @endif
+
+                    <button type="submit" class="btnSubmit">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +153,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,88 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    body{
+        background-color: #f3f5f7;
+    }
+
+    .lBox{
+        min-width: 0;
+        background-color: white;
+        box-shadow: #c7c7c7 0px 0px 30px;
+        padding: 30px;
+        border-radius: 15px;
+    }
+
+    .btnSubmit {
+        border-radius: 15px; 
+        padding: 5px 20px;
+        border: none;
+        color: white;
+        background-image: linear-gradient(273deg, #4eb3ff, #0074ff);
+        box-shadow: #c7c7c7 5px 0px 20px;
+        width: 100%;
+    }
+
+    input{
+        border-radius: 15px !important;
+        margin-bottom: 10px;    
+    }
+
+    .fPass{
+        float: right;
+        font-size: 12px;
+        color: rgb(109, 109, 109);
+        margin-top: 10px;
+        text-decoration: none;
+    }
+    
+</style>
+
 <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-xl-4 col-lg-6">
+            <div class="lBox">
+                <h1 class="text-center">Register</h1><br>
+
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+
+                    <label for="" class="text-bold"><strong>Name</strong></label>
+                    <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus name="name" id="">
+                    @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+
+                    <label for="" class="text-bold"><strong>Email Adress</strong></label>
+                    <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus name="email" id="">
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+
+                    <label for="" class=""><strong>Password</strong></label>
+                    <input type="password" name="password" id="" class="form-control @error('password') is-invalid @enderror" required autocomplete="new-password">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+
+                    <label for="" class=""><strong>Confirm Password</strong></label>
+                    <input type="password" name="password_confirmation" id="" class="form-control @error('password') is-invalid @enderror" required autocomplete="new-password"><br>
+
+                    <button type="submit" class="btnSubmit">Register</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -73,5 +154,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
